26cd287a59db820ea833a68159dac7cfd71c66e0
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2022-12-17 20:28:25 +0530

    #include<stdio.h> void abun(int n) {     int i,sum=0;     for(i=1;i<n;i++)     {         if(n%i==0)         {             sum=sum+i;         }     }     if(sum>n)     {         printf("True");     }     else     {         printf("False");     } } int main() {     int n;     scanf("%d",&n);     abun(n); }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void abun(int n)
+{
+    int i,sum=0;
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            sum=sum+i;
+        }
+    }
+    if(sum>n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    abun(n);
+}
