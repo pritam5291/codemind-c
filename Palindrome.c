9d7fd63a8d7430beef7636ce78bbddb6d78f67b3
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2022-12-21 23:50:47 +0530

    #include<stdio.h> int main() { 	int n,q,r,s=0; 	scanf("%d",&n); 	q=n; 	while(q>0) 	{ 		r=q%10; 		s=s*10+r; 		q=q/10; 	} 	if(s==n) 	{ 		printf("True"); 	} 	else 	{ 		printf("False"); 	} }

--- Palindrome.c
+++ Palindrome.c
@@ -1,26 +1,21 @@
 #include<stdio.h>
-void pal(int n)
-{
-    int r,s=0,q;
-    q=n;
-    while(q>0)
-    {
-        r=q%10;
-        s=s*10+r;
-        q=q/10;
-    }
-    if(s==n)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
-}
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    pal(n);
+	int n,q,r,s=0;
+	scanf("%d",&n);
+	q=n;
+	while(q>0)
+	{
+		r=q%10;
+		s=s*10+r;
+		q=q/10;
+	}
+	if(s==n)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
 }
