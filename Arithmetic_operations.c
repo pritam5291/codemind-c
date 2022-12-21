2159b44ac088d590e6f1c18cf598fa7e0ce88135
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2022-12-21 17:53:32 +0530

    #include<stdio.h> void art(int a,int b) {     int sum,diff,pro,quo,rem;     sum = a+b;     diff = a-b;     pro = a*b;     quo = a/b;     rem = a%b;     printf("Sum:%d Difference:%d Product:%d Quotient:%d Remainder:%d",sum,diff,pro,quo,rem); } int main() {     int a,b;     scanf("%d%d",&a,&b);     art(a,b); }

---
+++ Arithmetic_operations.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void art(int a,int b)
+{
+    int sum,diff,pro,quo,rem;
+    sum = a+b;
+    diff = a-b;
+    pro = a*b;
+    quo = a/b;
+    rem = a%b;
+    printf("Sum:%d
+Difference:%d
+Product:%d
+Quotient:%d
+Remainder:%d",sum,diff,pro,quo,rem);
+}
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    art(a,b);
+}
