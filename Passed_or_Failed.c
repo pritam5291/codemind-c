1ee8debfb8bfd90faa0363f39cc0816b49bf8f1e
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2022-12-21 18:32:00 +0530

    #include<stdio.h> void pf(int a,int b,int c,int d,int e) {     if(a>34 && b>34 && c>34 && d>34 && e>34)     {         printf("PASSED");     }     else     {         printf("FAILED");     } } int main() {     int a,b,c,d,e;     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);     pf(a,b,c,d,e); }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void pf(int a,int b,int c,int d,int e)
+{
+    if(a>34 && b>34 && c>34 && d>34 && e>34)
+    {
+        printf("PASSED");
+    }
+    else
+    {
+        printf("FAILED");
+    }
+}
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    pf(a,b,c,d,e);
+}
