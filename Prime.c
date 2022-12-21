a9da3a7660b392178959d9cc4a5aef4c82cbccd7
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2022-12-21 20:32:49 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2!=0)     {         printf("Prime");     }     else     {         printf("Not Prime");     } }

--- Prime.c
+++ Prime.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
-void pr(int n)
-{
-    if(n%2==0)
-    {
-        printf("Not Prime");
-    }
-    else
-    {
-        printf("Prime");
-    }
-}
 int main()
 {
     int n;
     scanf("%d",&n);
-    pr(n);
+    if(n%2!=0)
+    {
+        printf("Prime");
+    }
+    else
+    {
+        printf("Not Prime");
+    }
 }
