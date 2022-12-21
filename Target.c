90131b91103ae0a923e5f463394227140a1cfc13
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2022-12-21 18:14:27 +0530

    #include<stdio.h> void tar(int a,int b,int c,int d) {     if(a>=10 && b>=10 && c>=10 && d>=10)     {         printf("YES");     }     else     {         printf("NO");     } } int main() {     int a,b,c,d;     scanf("%d%d%d%d",&a,&b,&c,&d);     tar(a,b,c,d); }

---
+++ Target.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void tar(int a,int b,int c,int d)
+{
+    if(a>=10 && b>=10 && c>=10 && d>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    tar(a,b,c,d);
+}
