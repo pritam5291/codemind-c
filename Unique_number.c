64c384d5db056e8836a7b5035f28321c7deaec1f
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2023-05-24 13:28:22 +0530

    #include<stdio.h> int main() {
       int n, num, i, j, r, k = 0, flag = 1;
       int arr[100];
       scanf("%d", &n);
    
       num = n;
       while (n > 0) {
    
           r = n % 10;
           arr[k] = r;
           n = n / 10;
           k++;
       }
    
       for (i = 0; i < k; i++) {
           for (j = i + 1; j < k; j++) {
               if (arr[i] == arr[j]) {
                   flag = 0;
               }
           }
    
       }
    
       if (flag == 1) 
       {
           printf("Unique Number");
       } else {
           
       
           printf("Not Unique Number");
       }
       }

--- Unique_number.c
+++ Unique_number.c
@@ -5,9 +5,9 @@ int main()
    int n, num, i, j, r, k = 0, flag = 1;
    int arr[100];
    scanf("%d", &n);
+
    num = n;
-   while (n > 0)
-   {
+   while (n > 0) {
 
        r = n % 10;
        arr[k] = r;
@@ -15,23 +15,19 @@ int main()
        k++;
    }
 
-   for(i = 0; i < k; i++) {
-       for(j = i + 1; j < k; j++)
-       {
-           if(arr[i] == arr[j])
-           {
-               flag=0;
+   for (i = 0; i < k; i++) {
+       for (j = i + 1; j < k; j++) {
+           if (arr[i] == arr[j]) {
+               flag = 0;
            }
        }
 
    }
 
-   if(flag==1) 
+   if (flag == 1) 
    {
        printf("Unique Number");
-   }
-   else
-   {
+   } else {
        
    
        printf("Not Unique Number");
