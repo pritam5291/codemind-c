a5fdc89b4333350af49bf6ab467a4320616542f0
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2023-05-24 12:58:20 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=n;j>=1;j--)         {         printf("%d ",j);         }
        printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n,i,j;
     scanf("%d",&n);
-    for(i=n;i>0;i--)
+    for(i=n;i>=1;i--)
     {
-        for(j=1;j<=i;j++)
-        printf("%d",j);
+        for(j=n;j>=1;j--)
+        {
+        printf("%d ",j);
+        }
     
     printf("
 ");
